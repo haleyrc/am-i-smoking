1f15e70cba140b5c6fe974c00bfbe0d9e7ece8b8
Ryan Haley <ryanhaley@frazer.biz>
2018-05-30 19:35:18 -0400

    Update icons and fix scheduling bug with same day event

--- src/App.js
+++ src/App.js
@@ -14,123 +14,131 @@ import './style.css'
 export const Context = React.createContext()
 
 class App extends Component {
-  state = {
-    loading: true,
-    upcoming: true,
-    next: null,
-    pendingNext: null,
-    user: null
-  }
-
-  componentDidCatch = (error, info) => {
-    console.log({ error, info })
-  }
-
-  componentDidMount = async () => {
-    auth.onAuthStateChanged(async (lastUser) => {
-      if (lastUser) {
-        const userRef = firebase.database().ref('users').child(lastUser.uid)
-        userRef.on('value', (snapshot) => {
-          this.setState({ user: snapshot.val() })
-        })
-      }
-    })
-
-    const nextEventRef = firebase.database().ref('nextEvent')
-    nextEventRef.on('value', (snapshot) => {
-      const next = new Date(snapshot.val())
-      const now = new Date()
-      this.setState({
-        upcoming: next > now,
-        next: next,
-        loading: false
-      })
-    })
-  }
-
-  setNextEvent = (date) =>
-    firebase
-      .database()
-      .ref('nextEvent')
-      .set(
-        this.state.pendingNext
-          ? this.state.pendingNext.toString()
-          : moment().toString()
-      )
-
-  clearEvent = () => firebase.database().ref('nextEvent').remove()
-
-  login = async () => {
-    const result = await auth.signInWithPopup(provider)
-    let user = await firebase
-      .database()
-      .ref('users')
-      .child(result.user.uid)
-      .once('value')
-
-    if (!user.val()) {
-      if (!result.user.emailVerified) {
-        return
-      }
-
-      await firebase.database().ref('users').child(result.user.uid).set({
-        email: result.user.email,
-        name: result.user.displayName,
-        photo: result.user.photoURL,
-        admin: false,
-        friend: false
-      })
-
-      user = await firebase
-        .database()
-        .ref('users')
-        .child(result.user.uid)
-        .once('value')
+    state = {
+        loading: true,
+        upcoming: true,
+        next: null,
+        pendingNext: null,
+        user: null
     }
 
-    this.setState({ user: user.val() })
-  }
+    componentDidCatch = (error, info) => {
+        console.log({ error, info })
+    }
 
-  logout = () => auth.signOut().then(() => this.setState({ user: null }))
+    componentDidMount = async () => {
+        auth.onAuthStateChanged(async (lastUser) => {
+            if (lastUser) {
+                const userRef = firebase.database().ref('users').child(lastUser.uid)
+                userRef.on('value', (snapshot) => {
+                    this.setState({ user: snapshot.val() })
+                })
+            }
+        })
 
-  render () {
-    const { loading, user, upcoming, next } = this.state
-    return loading ? (
-      <Splash />
-    ) : (
-      <Context.Provider value={this.state.user}>
-        <Main>
-          <AppBar user={user} onLogout={this.logout} onLogin={this.login} />
-          {user &&
-          user.admin && (
-            <Scheduler
-              onChange={(date) => this.setState({ pendingNext: date })}
-              onClear={this.clearEvent}
-              onSchedule={this.setNextEvent}
-            />
-          )}
-          {upcoming ? (
-            <React.Fragment>
-              <Cards>
-                <TimeCard next={next.toString()} />
-                <ConnectedCard subject='Meat' />
-                <ConnectedCard subject='Sides' />
-                <ConnectedCard subject='Beer' />
-              </Cards>
-              {user && user.friend && <Map />}
-            </React.Fragment>
-          ) : (
-            <div>No Upcoming Smoking Events</div>
-          )}
-        </Main>
-      </Context.Provider>
-    )
-  }
+        const nextEventRef = firebase.database().ref('nextEvent')
+        nextEventRef.on('value', (snapshot) => {
+            const next = new Date(snapshot.val())
+            const now = new Date()
+            this.setState({
+                upcoming: moment(next).isAfter(moment(now).startOf('day')),
+                next: next,
+                loading: false
+            })
+        })
+    }
+
+    setNextEvent = (date) =>
+        firebase
+            .database()
+            .ref('nextEvent')
+            .set(
+                this.state.pendingNext
+                    ? this.state.pendingNext.toString()
+                    : moment().toString()
+            )
+
+    clearEvent = () => firebase.database().ref('nextEvent').remove()
+
+    login = async () => {
+        const result = await auth.signInWithPopup(provider)
+        let user = await firebase
+            .database()
+            .ref('users')
+            .child(result.user.uid)
+            .once('value')
+
+        if (!user.val()) {
+            if (!result.user.emailVerified) {
+                return
+            }
+
+            await firebase.database().ref('users').child(result.user.uid).set({
+                email: result.user.email,
+                name: result.user.displayName,
+                photo: result.user.photoURL,
+                admin: false,
+                friend: false
+            })
+
+            user = await firebase
+                .database()
+                .ref('users')
+                .child(result.user.uid)
+                .once('value')
+        }
+
+        this.setState({ user: user.val() })
+    }
+
+    logout = () => auth.signOut().then(() => this.setState({ user: null }))
+
+    render() {
+        const { loading, user, upcoming, next } = this.state
+        return loading ? (
+            <Splash />
+        ) : (
+                <Context.Provider value={this.state.user}>
+                    <Main>
+                        <AppBar user={user} onLogout={this.logout} onLogin={this.login} />
+                        {user &&
+                            user.admin && (
+                                <Scheduler
+                                    onChange={(date) => this.setState({ pendingNext: date })}
+                                    onClear={this.clearEvent}
+                                    onSchedule={this.setNextEvent}
+                                />
+                            )}
+                        {upcoming ? (
+                            <React.Fragment>
+                                <Cards>
+                                    <TimeCard next={next.toString()} />
+                                    <ConnectedCard subject='Meat' />
+                                    <ConnectedCard subject='Sides' />
+                                    <ConnectedCard subject='Beer' />
+                                </Cards>
+                                {user && user.friend && <Map />}
+                            </React.Fragment>
+                        ) : (
+                                <Notice>No Upcoming Smoking Events</Notice>
+                            )}
+                    </Main>
+                </Context.Provider>
+            )
+    }
 }
 
 export default App
 
-const Splash = styled('div')`
+const Notice = styled('div') `
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 2em;
+`
+
+const Splash = styled('div') `
   height: 100vh;
   background: url('images/splash_sm.jpg');
   background-size: cover;
@@ -138,7 +146,7 @@ const Splash = styled('div')`
   filter: blur(10px);
 `
 
-const Cards = styled('div')`
+const Cards = styled('div') `
   display: grid;
   flex-direction: 1fr;
   padding: 0 20px;
@@ -150,7 +158,7 @@ const Cards = styled('div')`
   }
 `
 
-const Main = styled('div')`
+const Main = styled('div') `
   padding-bottom: 30px;
   display: flex;
   flex-direction: column;
--- src/Card.js
+++ src/Card.js
@@ -5,86 +5,94 @@ import firebase from 'firebase'
 import { Context } from './App'
 
 class ConnectedCard extends Component {
-  state = {
-    loading: true,
-    items: {},
-    ref: null
-  }
-
-  componentDidMount = () => {
-    console.log(`${this.props.subject} mounted`)
-    const ref = firebase.database().ref(this.props.subject.toLowerCase())
-    ref.on('value', (snapshot) => {
-      this.setState({ ref, loading: false, items: snapshot.val() || {} }, () =>
-        console.log(`${this.props.subject} loaded from firebase`)
-      )
-    })
-  }
-
-  componentWillUnmount = () => {
-    this.state.ref.off('value')
-  }
-
-  removeItem = (key) => {
-    this.state.ref.child(key).remove()
-  }
-
-  addItem = () => {
-    const item = prompt(
-      `What ${this.props.subject.toLowerCase()} would you like to add?`,
-      ''
-    )
-    if (item == null || item === '') {
-      return
+    state = {
+        loading: true,
+        items: {},
+        ref: null
     }
-    this.state.ref.push(item)
-  }
 
-  render () {
-    const { items, loading } = this.state
-    const subject = this.props.subject
+    componentDidMount = () => {
+        console.log(`${this.props.subject} mounted`)
+        const ref = firebase.database().ref(this.props.subject.toLowerCase())
+        ref.on('value', (snapshot) => {
+            this.setState({ ref, loading: false, items: snapshot.val() || {} }, () =>
+                console.log(`${this.props.subject} loaded from firebase`)
+            )
+        })
+    }
 
-    return (
-      <Card>
-        <CardHeader image={subject.toLowerCase()}>
-          <CardTitle>{subject}</CardTitle>
-        </CardHeader>
-        <CardBody>
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ItemList>
-              {Object.keys(items).length ? (
-                Object.keys(items).map((key) => (
-                  <Item key={key} onClick={() => this.removeItem(key)}>
-                    {items[key]}
-                  </Item>
-                ))
-              ) : (
-                <Item>No {subject.toLowerCase()} added yet.</Item>
-              )}
-            </ItemList>
-          )}
-        </CardBody>
-        <CardFooter>
-          <Context.Consumer>
-            {(user) => {
-              console.log('context user:', user)
-              return (
-                user &&
-                user.admin && <NewItem onClick={this.addItem}>+</NewItem>
-              )
-            }}
-          </Context.Consumer>
-        </CardFooter>
-      </Card>
-    )
-  }
+    componentWillUnmount = () => {
+        this.state.ref.off('value')
+    }
+
+    removeItem = (key) => {
+        this.state.ref.child(key).remove()
+    }
+
+    addItem = () => {
+        const item = prompt(
+            `What ${this.props.subject.toLowerCase()} would you like to add?`,
+            ''
+        )
+        if (item == null || item === '') {
+            return
+        }
+        this.state.ref.push(item)
+    }
+
+    render() {
+        const { items, loading } = this.state
+        const subject = this.props.subject
+
+        return (
+            <Card>
+                <CardHeader image={subject.toLowerCase()}>
+                    <CardTitle>{subject}</CardTitle>
+                </CardHeader>
+                <CardBody>
+                    {loading ? (
+                        <div>Loading...</div>
+                    ) : (
+                            <ItemList>
+                                {Object.keys(items).length ? (
+                                    Object.keys(items).map((key) => (
+                                        <Context.Consumer key={key}>
+                                            {(user) => (
+                                                <Item
+                                                    key={key}
+                                                    onClick={() =>
+                                                        user && user.admin && this.removeItem(key)}
+                                                >
+                                                    {items[key]}
+                                                </Item>
+                                            )}
+                                        </Context.Consumer>
+                                    ))
+                                ) : (
+                                        <Item>No {subject.toLowerCase()} added yet.</Item>
+                                    )}
+                            </ItemList>
+                        )}
+                </CardBody>
+                <CardFooter>
+                    <Context.Consumer>
+                        {(user) => {
+                            console.log('context user:', user)
+                            return (
+                                user &&
+                                user.admin && <NewItem onClick={this.addItem}>+</NewItem>
+                            )
+                        }}
+                    </Context.Consumer>
+                </CardFooter>
+            </Card>
+        )
+    }
 }
 
 export default ConnectedCard
 
-export const CardBody = styled('div')`
+export const CardBody = styled('div') `
   padding-top: 20px;
   padding-bottom: 20px;
   color: white;
@@ -96,21 +104,21 @@ export const CardBody = styled('div')`
   flex: 1;
 `
 
-export const Card = styled('div')`
+export const Card = styled('div') `
   background-color: #212121;
   box-shadow: 0 10px 10px rgba(0, 0, 0, .26);
   display: flex;
   flex-direction: column;
 `
 
-export const CardHeader = styled('div')`
+export const CardHeader = styled('div') `
   background-color: #424242;
   background: url('${(props) => `images/${props.image}_sm.jpg`}');
   background-size: cover;
   background-position: center;
 `
 
-export const CardTitle = styled('h2')`
+export const CardTitle = styled('h2') `
   font-family: 'Cabin Sketch', cursive;
   color: rgba(255,255,255,1);
   font-size: 48px;
@@ -131,7 +139,7 @@ export const CardTitle = styled('h2')`
   }
 `
 
-const NewItem = styled('button')`
+const NewItem = styled('button') `
   background: none;
   border: none;
   font-family: 'Cabin Sketch', cursive;
@@ -144,17 +152,17 @@ const NewItem = styled('button')`
   padding: 0 10px;
 `
 
-const ItemList = styled('ul')`
+const ItemList = styled('ul') `
   list-style-type: none;
 `
 
-const Item = styled('li')`
+const Item = styled('li') `
   @media(min-width: 1000px) {
     font-size: 24px;
   }
 `
 
-const CardFooter = styled('div')`
+const CardFooter = styled('div') `
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
